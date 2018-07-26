8baa8d4c3cbf808718394a9f49537197f1a18956
summer <15733207728@163.com>
2018-07-26 22:59:58 +0800

    'music'

---
+++ dist/precache-manifest.1e6f78e289a342063819f667337a72c7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5ad80c8524c2d65e657",
+    "url": "/js/6.c5ad80c8.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3a3b84a8ebc9abb2c3fd",
+    "url": "/css/7.738d08b1.css"
+  },
+  {
+    "revision": "1ece5963f1de133955e6",
+    "url": "/css/app.453e5000.css"
+  },
+  {
+    "revision": "20d1b09cb3cce6cfdbc2856b8e5c1659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4e652422f9587a48969",
+    "url": "/js/2.c4e65242.js"
+  },
+  {
+    "revision": "854cc0dd48a18fc60fd8e35466f7bf98",
+    "url": "/fonts/iconfont.854cc0dd.ttf"
+  },
+  {
+    "revision": "cd846a0a5938eebe4283",
+    "url": "/js/3.cd846a0a.js"
+  },
+  {
+    "revision": "198f2c7c4c8496979bdcac581680f99b",
+    "url": "/img/iconfont.198f2c7c.svg"
+  },
+  {
+    "revision": "2dac185731e5e8fff657",
+    "url": "/js/4.2dac1857.js"
+  },
+  {
+    "revision": "7324d319447f49a21384deedb880be12",
+    "url": "/fonts/iconfont.7324d319.eot"
+  },
+  {
+    "revision": "248a924548db1931ea9c",
+    "url": "/js/5.248a9245.js"
+  },
+  {
+    "revision": "f01a252389c26bcf016816242eaa6aee",
+    "url": "/img/hot_music_bg_2x.f01a2523.jpg"
+  },
+  {
+    "revision": "af9e95fa241236d6b953",
+    "url": "/js/0.af9e95fa.js"
+  },
+  {
+    "revision": "1ece5963f1de133955e6",
+    "url": "/js/app.1ece5963.js"
+  },
+  {
+    "revision": "3a3b84a8ebc9abb2c3fd",
+    "url": "/js/7.3a3b84a8.js"
+  },
+  {
+    "revision": "e58c0b7eda0cdbc43c0c",
+    "url": "/js/chunk-vendors.e58c0b7e.js"
+  },
+  {
+    "revision": "d3bdd1080a72129346aa0b4b4964b75f",
+    "url": "/img/disc.d3bdd108.png"
+  },
+  {
+    "revision": "be4ebbeb6befadfcae75ce174e7db862",
+    "url": "/img/needle-ip6.be4ebbeb.png"
+  },
+  {
+    "revision": "5207a28c3767992ca4bb6d4887c74880",
+    "url": "/img/index_icon_2x.5207a28c.png"
+  },
+  {
+    "revision": "c5ad80c8524c2d65e657",
+    "url": "/css/6.7b839a6b.css"
+  },
+  {
+    "revision": "248a924548db1931ea9c",
+    "url": "/css/5.a1a31750.css"
+  },
+  {
+    "revision": "2dac185731e5e8fff657",
+    "url": "/css/4.ba20c673.css"
+  },
+  {
+    "revision": "cd846a0a5938eebe4283",
+    "url": "/css/3.3801d112.css"
+  },
+  {
+    "revision": "c4e652422f9587a48969",
+    "url": "/css/2.49986b4c.css"
+  },
+  {
+    "revision": "af9e95fa241236d6b953",
+    "url": "/css/0.e7c30abe.css"
+  }
+];
